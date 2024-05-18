ec4c6f49789399cbdc3b2a71c90bbfcde7040bd5
Ritik Yadav <ritikyadavwork23@gmail.com>
2024-05-19 01:16:06 +0530

    create the view and fetch books api

--- bookapiapp/views.py
+++ bookapiapp/views.py
@@ -1,3 +1,15 @@
+import requests
+from django.http import JsonResponse
 from django.shortcuts import render
+from .api import fetch_books_api
 
-# Create your views here.
+
+def search_books(request):
+    query = request.GET.get('q', 'coding')
+    books = fetch_books_api(query)
+
+    if 'error' in books:
+        return JsonResponse(books, status=400)
+
+    return render(request, 'search.html', {'books': books, 'query': query})
+
