1915ce7c7aaada39275b80c050d472c54bbc8a47
Ritik Yadav <ritikyadavwork23@gmail.com>
2024-05-19 17:09:30 +0530

    added fied in model

--- bookapiapp/models.py
+++ bookapiapp/models.py
@@ -2,18 +2,26 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Book(models.Model):
+class Recommendation(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     description = models.TextField()
-    genre = models.CharField(max_length=100)
-    publication_date = models.DateField()
-    rating = models.DecimalField(max_digits=3, decimal_places=2)
+    cover_image = models.URLField()
+    genre = models.CharField(max_length=100, blank=True, null=True)
+    publication_date = models.DateField(blank=True, null=True)
+    rating = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
 
 class UserInteraction(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    book = models.ForeignKey(Recommendation, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     liked = models.BooleanField(default=False)
     comment = models.TextField(blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} interaction with {self.book.title}"
