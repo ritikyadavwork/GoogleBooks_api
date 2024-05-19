7ed544cb2cae6a3f83e7fd9d56af7b380d910b76
Ritik Yadav <ritikyadavwork23@gmail.com>
2024-05-19 13:03:57 +0530

    create the model name Book And UserInteraction

--- bookapiapp/models.py
+++ bookapiapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    description = models.TextField()
+    genre = models.CharField(max_length=100)
+    publication_date = models.DateField()
+    rating = models.DecimalField(max_digits=3, decimal_places=2)
+
+
+class UserInteraction(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    liked = models.BooleanField(default=False)
+    comment = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
