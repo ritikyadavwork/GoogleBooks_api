47418a54425a09ea8dca24ddd531be257568caf9
Ritik Yadav <ritikyadavwork23@gmail.com>
2024-05-19 13:03:20 +0530

    error handling for API requests and responses

--- bookapiapp/api.py
+++ bookapiapp/api.py
@@ -2,21 +2,46 @@ import requests
 from django.http import JsonResponse
 
 
-def fetch_books_api(query):
+def fetch_books_api(query=None, author=None, category=None):
     if not query:
         return {'error': 'No query parameter provided'}
 
     api_url = 'https://www.googleapis.com/books/v1/volumes'
+
+    search_query = ''
+
+    if query:
+        search_query += query
+    if author:
+        if search_query:
+            search_query += '+'
+        search_query += f'inauthor:{author}'
+    if category:
+        if search_query:
+            search_query += '+'
+        search_query += f'subject:{category}'
 
     params = {
         'q': query,
         'key': 'AIzaSyAdScHOuR0EWiwYvzib7VCQ738zw3cl54I'
     }
 
-    response = requests.get(api_url, params=params)
+    try:
+        response = requests.get(api_url, params=params)
+        response.raise_for_status()  # Raise HTTPError for bad responses
+    except requests.exceptions.HTTPError as http_err:
+        return {'error': f'HTTP error occurred: {http_err}'}
+    except requests.exceptions.ConnectionError as conn_err:
+        return {'error': f'Connection error occurred: {conn_err}'}
+    except requests.exceptions.Timeout as timeout_err:
+        return {'error': f'Timeout error occurred: {timeout_err}'}
+    except requests.exceptions.RequestException as req_err:
+        return {'error': f'Request error occurred: {req_err}'}
 
     if response.status_code == 200:
         data = response.json()
+        if 'items' not in data:
+            return {'error': 'No books found'}
         books = [
             {
                 'title': item['volumeInfo'].get('title'),
