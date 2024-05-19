703381b018e0bad036fb931fdaeda3a8bed97f9a
Ritik Yadav <ritikyadavwork23@gmail.com>
2024-05-19 17:08:01 +0530

    change the book to Recommendation

--- bookapiapp/admin.py
+++ bookapiapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Book, UserInteraction
+from .models import Recommendation, UserInteraction
 
-admin.site.register(Book)
+admin.site.register(Recommendation)
 admin.site.register(UserInteraction)
