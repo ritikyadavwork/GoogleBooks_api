ad8d220b55640c4077b8991081af6ee4d430bab9
Ritik Yadav <ritikyadavwork23@gmail.com>
2024-05-19 17:10:18 +0530

    added the url

--- bookapiapp/urls.py
+++ bookapiapp/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path, include
 from .views import search_books
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet, UserInteractionViewSet
+from .views import get_recommendations, submit_recommendation, \
+    filter_recommendations, user_interaction
 
 app_name = 'bookapiapp'
 
-router = DefaultRouter()
-router.register(r'books', BookViewSet)
-router.register(r'interactions', UserInteractionViewSet)
-
 urlpatterns = [
-    path('', include(router.urls)),
     path('search/', search_books, name='search_books'),
+    path('recommendations/', get_recommendations, name='get_recommendations'),
+    path('recommendations/submit/', submit_recommendation, name='submit_recommendation'),
+    path('recommendations/filter/', filter_recommendations, name='filter_recommendations'),
+    path('interaction/', user_interaction, name='user_interaction'),
 
 ]
