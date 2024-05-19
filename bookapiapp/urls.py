b4105529618406148788f42f27d10cff5fc900ee
Ritik Yadav <ritikyadavwork23@gmail.com>
2024-05-19 13:05:06 +0530

    create the url for the bookview set

--- bookapiapp/urls.py
+++ bookapiapp/urls.py
@@ -1,10 +1,16 @@
-from django.contrib import admin
 from django.urls import path, include
 from .views import search_books
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet, UserInteractionViewSet
 
 app_name = 'bookapiapp'
 
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
+router.register(r'interactions', UserInteractionViewSet)
+
 urlpatterns = [
+    path('', include(router.urls)),
     path('search/', search_books, name='search_books'),
 
 ]
